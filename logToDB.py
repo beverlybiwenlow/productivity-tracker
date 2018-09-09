b9d4bcd14fb05ddc48fe9a6d9f5d3990719c2712
Beverly <beverlybiwenlow@hotmail.com>
2018-09-08 18:46:59 -0700

    log to DB

--- logToDB.py
+++ logToDB.py
@@ -1,3 +1,11 @@
-# connect to mongo
-# scrape log, change to json
-# upload to db
+import json
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://admin:password123@ds149252.mlab.com:49252/fbhackathon")
+
+# Get the sampleDB database
+db = client['fbhackathon']
+
+
+db.test.insert({"testval": {"first": 1, "second":2}})
+# db.close()
