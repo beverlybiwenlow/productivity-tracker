612ff233b587be771711d439a7db279a15a957d2
Beverly <beverlybiwenlow@hotmail.com>
2018-09-08 18:54:55 -0700

    formatting to json

--- logToDB.py
+++ logToDB.py
@@ -1,11 +1,12 @@
 import json
 from pymongo import MongoClient
 
+# DB details
 client = MongoClient("mongodb://admin:password123@ds149252.mlab.com:49252/fbhackathon")
-
-# Get the sampleDB database
 db = client['fbhackathon']
 
-
-db.test.insert({"testval": {"first": 1, "second":2}})
-# db.close()
+# JSON to store
+page = open("logs/log.json", 'r')
+parsed = json.loads(page.read())
+print(parsed)
+db.test.insert(parsed)
